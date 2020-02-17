322074fe0f497802beaf6ac13ee47f6dbb0b2e76
Oleksii Ahieiev <djamply@gmail.com>
2020-02-17 04:12:32 +0200

    md5: String (-s) hashing

--- ft_strnew.c
+++ ft_strnew.c
@@ -18,12 +18,8 @@ char	*ft_strnew(size_t size)
 
 	str = (char *)malloc(sizeof(char) * (size + 1));
 	if (str)
-	{
-		while (size > 0)
-			str[size--] = '\0';
-		str[0] = '\0';
-	}
+		ft_bzero(str, size + 1);
 	else
-		return (0);
+		return (NULL);
 	return (str);
 }
